c30748e4928f9e93bf95a1c0e31c78ce9a15b0f9
Izaac Mansfield <35855284+izaacmansfield@users.noreply.github.com>
2023-03-27 14:08:36 -0700

    added logic for login and account creation within same php + js files

---
+++ user_actions.php
@@ -0,0 +1,48 @@
+<?php
+header('Content-Type: application/json');
+
+$servername = "107.180.1.16";
+$username = "sprc2023team3";
+$password = "sprc2023team3";
+$dbname = "sprc2023team3";
+
+$action = $_POST['action'];
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+$email = $_POST['email'];
+$user_password = $_POST['password'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
+}
+
+if ($action === 'login') {
+    $sql = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    if ($row) {
+        echo json_encode($row);
+    } else {
+        echo json_encode(['error' => 'User not found']);
+    }
+} elseif ($action === 'create_account') {
+    $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $name, $email, $user_password);
+
+    if ($stmt->execute()) {
+        echo json_encode(['success' => 'User created successfully']);
+    } else {
+        echo json_encode(['error' => 'Error: ' . $stmt->error]);
+    }
+} else {
+    echo json_encode(['error' => 'Invalid action']);
+}
+
+$conn->close();
+?>
